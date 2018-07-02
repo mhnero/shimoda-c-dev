2a56ed10196c08565bb3e5c5e8ce773cdd9d1edd
Nkmha <s1642126lq@s.chibakoudai.jp>
2018-07-02 23:35:33 +0900

    review

--- sample/shop/shop_product.php
+++ sample/shop/shop_product.php
@@ -1,10 +1,30 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['member_login'])==false)
+{
+	print 'ようこそゲスト様　';
+	print '<a href="member_login.html">会員ログイン</a><br />';
+	print '<br />';
+}
+else
+{
+	print 'ようこそ';
+	print $_SESSION['member_name'];
+	print '様　';
+	print '<a href="member_logout.php">ログアウト</a><br />';
+	print '<br />';
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>勉強会</title>
+<title>ろくまる農園</title>
 </head>
 <body>
+
 <?php
 
 try
@@ -23,6 +43,10 @@ $stmt=$dbh->prepare($sql);
 $data[]=$pro_code;
 $stmt->execute($data);
 
+$sql2='SELECT name,score,comment FROM mst_review WHERE code=?';
+$stmt2=$dbh->prepare($sql2);
+$stmt2->execute($data);
+
 $rec=$stmt->fetch(PDO::FETCH_ASSOC);
 $pro_name=$rec['name'];
 $pro_price=$rec['price'];
@@ -39,14 +63,12 @@ else
 	$disp_gazou='<img src="../product/gazou/'.$pro_gazou_name.'">';
 }
 print '<a href="shop_cartin.php?procode='.$pro_code.'">カートに入れる</a><br /><br />';
-
 }
 catch(Exception $e)
 {
 	print'ただいま障害により大変ご迷惑をお掛けしております。';
 	exit();
 }
-
 ?>
 
 商品情報参照<br />
@@ -63,8 +85,27 @@ catch(Exception $e)
 <?php print $disp_gazou; ?>
 <br />
 <br />
+<?php print '<a href="shop_add.php?procode='.$pro_code.'">レビューを入力する</a>'?>
+<br />
+レビュー一覧<br/>
+<?php
+while(true)
+{
+	$rec2=$stmt2->fetch(PDO::FETCH_ASSOC);
+	if($rec2==false)
+	{
+		break;
+	}
+	print '●';
+	print $rec2['name'].'---';
+	print $rec2['score'].'点<br/>';
+	print $rec2['comment'].'<br/>';
+	print '<br />';
+}
+?>
 <form>
 <input type="button" onclick="history.back()" value="戻る">
 </form>
+
 </body>
 </html>
