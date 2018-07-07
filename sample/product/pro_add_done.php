2b8ede3e3e5389785057e23450b7e9952448f894
Nero <s1642078dm@s.chibakoudai.jp>
2018-07-08 00:29:52 +0900

    20180707

--- sample/product/pro_add_done.php
+++ sample/product/pro_add_done.php
@@ -38,11 +38,21 @@ $pro_type=$post['type'];
 $pro_seisan=$post['seisan'];
 $pro_pricelevel=$post['pricelevel'];
 
-$dsn='mysql:dbname=shop2;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+if (DEBUG) {
+	$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+	$user='root';
+	$password='';
+	$dbh=new PDO($dsn,$user,$password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}
+	else{
+	$dbServer = '127.0.0.1';
+	$dbUser = $_SERVER['MYSQL_USER'];
+	$dbPass = $_SERVER['MYSQL_PASSWORD'];
+	$dbName = $_SERVER['MYSQL_DB'];
+	$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+	$dbh = new PDO($dsn, $dbUser, $dbPass);
+	}
 
 $sql='INSERT INTO mst_product(name,price,gazou,type,seisan,pricelevel) VALUES (?,?,?,?,?,?)';
 $stmt=$dbh->prepare($sql);
